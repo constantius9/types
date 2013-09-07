6ccc48c9000a44727269e1b164f486a3d5be0636
Michael Pankov <constantius9@gmail.com>
2013-09-08 01:40:53 +0400

    Add eval1 function

--- arithmetic/arithmetic.hs
+++ arithmetic/arithmetic.hs
@@ -21,6 +21,29 @@ newtype Line = Line Int
 
 newtype Col = Col Int
 
+dummyinfo =
+    Info(
+        Span(
+            (Begin(
+                Pos(
+                    Line(
+                        (-1) ),
+                    Col(
+                        (-1) )
+                )
+            ) ),
+            (End  (
+                Pos(
+                    Line(
+                        (-1) ),
+                    Col(
+                        (-1) )
+                )
+            ) )
+        )
+    )
+
+
 isNumericVal (TmZero _) = True
 isNumericVal (TmSucc _ t1) = isNumericVal t1
 isNumericVal _ = False
@@ -30,3 +53,24 @@ isVal t = case t of
     (TmFalse _) -> True
     t | isNumericVal t -> True
     _ -> False
+
+eval1 t = case t of
+    TmIf _ (TmTrue _) t2 _ -> t2
+    TmIf _ (TmFalse _) _ t3 -> t3
+    TmIf fi t1 t2 t3 ->
+        let t1' = eval1 t1
+        in TmIf fi t1' t2 t3
+    TmSucc fi t1 ->
+        let t1' = eval1 t1
+        in TmSucc fi t1'
+    TmPred _ (TmZero _) -> TmZero dummyinfo
+    TmPred _ (TmSucc _ nv1) | isNumericVal nv1 -> nv1
+    TmPred fi t1 ->
+        let t1' = eval1 t1
+        in TmPred fi t1'
+    TmIsZero _ (TmZero _) -> TmTrue dummyinfo
+    TmIsZero _ (TmSucc _ nv1) | isNumericVal nv1 -> TmFalse dummyinfo
+    TmIsZero fi t1 ->
+        let t1' = eval1 t1
+        in TmIsZero fi t1'
+    _ -> undefined
